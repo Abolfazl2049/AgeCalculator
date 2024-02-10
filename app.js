f2a53026d070e1ce98a526b66ff350c6a90cd6e3
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2024-02-10 21:50:34 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+let day, month, year, dayN, monthN, yearN, hourN, mintueN, secondN, birthdate;
+
+function inputGrabber() {
+  day = parseInt(document.getElementById("Day").value);
+  month = parseInt(document.getElementById("Month").value);
+  year = parseInt(document.getElementById("Year").value);
+  birthdate = new Date(year, month - 1, day);
+}
+function updateAge(day, month, year) {
+  if (
+    typeof day === "number" &&
+    typeof month === "number" &&
+    typeof year === "number" &&
+    year < 2024 &&
+    day <= 31 &&
+    month <= 12 &&
+    year >= 1800
+  ) {
+    setInterval(function () {
+      let dateNow = new Date();
+      secondN = dateNow.getSeconds();
+      mintueN = dateNow.getMinutes();
+      hourN = dateNow.getHours();
+      yearN = dateNow.getFullYear() - birthdate.getFullYear();
+      dayN = dateNow.getDate() - birthdate.getDate();
+      monthN = dateNow.getMonth() - birthdate.getMonth();
+
+      if (dayN < 0) {
+        monthN--;
+        dayN += new Date(birthdate.getFullYear() + 1, 0, 0).getDate();
+      }
+      if (monthN < 0) {
+        yearN--;
+        monthN += 12;
+      }
+
+      document.querySelector(".firstP").textContent = "";
+      document.getElementById("year-R").innerText = yearN;
+      document.getElementById("month-R").innerText = monthN;
+      document.getElementById("day-R").innerText = dayN;
+      document.getElementById("hour-R").innerText = hourN;
+      document.getElementById("mintue-R").innerText = mintueN;
+      document.getElementById("second-R").innerText = secondN;
+    }, 1000);
+  } else {
+    document.querySelector(".firstP").innerText = "*please Enter a valid date";
+  }
+}
+
+inputGrabber();
+function expand() {
+  if (screen.availWidth < 550) {
+    if (document.querySelector(".container").style.height === "770px") {
+      document.querySelector(".container").style.height = "500px";
+      document.querySelector(".img-1").src =
+        "assets/images/arrow_downward_FILL0_wght400_GRAD0_opsz24.svg";
+      document.querySelector("#hour-R").style.margin = "50px";
+      document.querySelector(".but-2").style.bottom = "unset";
+      document.querySelector(".secondp").innerText = "Expand";
+    } else {
+      document.querySelector(".but-2").style.bottom = "10px";
+      document.querySelector(".container").style.height = "770px";
+      document.querySelector("#hour-R").style.margin = "0";
+      document.querySelector(".img-1").src =
+        "assets/images/arrow_upward_FILL0_wght400_GRAD0_opsz24.svg";
+      document.querySelector(".secondp").innerText = "Collapse";
+    }
+  } else {
+    if (document.querySelector(".container").style.height === "800px") {
+      document.querySelector(".container").style.height = "530px";
+      document.querySelector(".img-1").src =
+        "assets/images/arrow_downward_FILL0_wght400_GRAD0_opsz24.svg";
+      document.querySelector("#hour-R").style.margin = "50px";
+      document.querySelector(".but-2").style.bottom = "unset";
+      document.querySelector(".secondp").innerText = "Expand";
+    } else {
+      document.querySelector(".but-2").style.bottom = "10px";
+      document.querySelector(".container").style.height = "800px";
+      document.querySelector("#hour-R").style.margin = "0";
+      document.querySelector(".img-1").src =
+        "assets/images/arrow_upward_FILL0_wght400_GRAD0_opsz24.svg";
+      document.querySelector(".secondp").innerText = "Collapse";
+    }
+  }
+}
